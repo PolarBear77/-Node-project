5bf29b0382d241822dbfa4e0cf84bc63eff69dbe
Won-Joon Choi <cwj5050@gmail.com>
2019-01-07 00:06:01 +0900

    Update 2_async_example.js

--- 2_async_example.js
+++ 2_async_example.js
@@ -1,3 +1,16 @@
+/*
+Example usage of async for sequential execution
+async is a library that supporst asynchronous pattens, in this example we will look at the two of their two sequential modules.
+	
+	1. waterfall (aka series or sequence)
+	With async.waterfall we can streamline tasks without using nested callbacks. Some simple functions may be suitable for callbacks,
+	but with a large number of callbacks management can become hard. waterfall helps in such cases. The results are passed as an array
+	to the last callback 
+
+	2. series
+	Functions are called sequentially, and results can be optionally passed as an array to the last callback.
+*/
+
 var fs = require("fs"),
     async = requre ("async").
     _dir = './data';
@@ -7,12 +20,24 @@ var writeStream = fs.creatWriteStream('./log.txt',
 	  'encoding' : 'utf8',
 	  'mode': 0666});
 
-sync.waterfall([ // array of tasks to be done sequently
-	function readDir(callback){
-		fs.readdir(_dir, function(err, files){
-			files.forEach(function(name){
-				callback(null, name);
-			});
-		},
+sync.waterfall([ // array of tasks to be done sequently, if this was done with callbacks we would have 5 nested functions!
+	
 
-//	function
+	function readDir(callback){
+		fs.readdir(_dir, function(err, files){ // err raises 
+				callback(null, name);
+		});
+	},
+
+	function loopFiles(files, callback){
+		files.forEach(function(name){
+			callback(null, name);
+		});
+	},
+
+	function checkFile(file, callback){
+		fs.stat(_dir + file, function(err, stats){
+			callback(err, stats, file);
+		});
+	},
+]
